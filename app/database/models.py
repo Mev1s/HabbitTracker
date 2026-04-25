f8e6f5be542440acb1639d21baec032f499ce1a1
Mev1s <Mev1s@users.noreply.github.com>
2026-04-25 14:46:44 +0700

    feat: new models: UsersOrm, HabitsOrm

--- app/database/models.py
+++ app/database/models.py
@@ -1,13 +1,30 @@
 from sqlalchemy import (
     Table,
     Column,
-    Integer,
-    String,
     MetaData,
     BigInteger,
     ForeignKey,
-    Date,
 )
 from datetime import date
+from sqlalchemy.orm import mapped_column, Mapped
+from typing import Annotated
 
 from database import Base
+
+# custom data types
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+class UsersOrm(Base):
+    __tablename__ = "users"
+
+    id: Mapped[intpk]
+    chat_id: Mapped[int] = mapped_column(BigInteger)
+    telegram_id: Mapped[int] = mapped_column(BigInteger)
+
+class HabitsOrm(Base):
+    __tablename__ = "habits"
+    id: Mapped[intpk]
+    title: Mapped[str]
+    description: Mapped[str | None]
+    created_at: Mapped[date] = mapped_column(default=date.today)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
