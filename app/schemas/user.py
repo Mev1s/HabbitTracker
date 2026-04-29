1a2b78bde333aa13a5aff6494751b048ccface0c
Mev1s <Mev1s@users.noreply.github.com>
2026-04-29 20:16:44 +0700

    feat: added schema user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,3 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated
+from datetime import datetime
 
 class UserBase(BaseModel):
+    telegram_id: Annotated[int, Field(description="Telegram unique user id", gt=0)]
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    account_created_at: Annotated[datetime, Field(description="Account created time")]
